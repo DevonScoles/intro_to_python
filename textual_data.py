63eac1abbe8f886167a2860f083ba131597dc90f
Nagadarshan Nanjundaswamy <nananjun@iu.edu>
2022-03-29 18:29:29 -0400

    03/29/2022

--- textual_data.py
+++ textual_data.py
@@ -1,13 +1,30 @@
-print('<'*25,"Begining",'>'*25) # use '\n' to create a new line inside of a string
+from email import message
 
+
+print('<'*25,"Begining",'>'*25) # use '\n' to create a new line inside of a string
+print ("Hello World\n")
 
 print("="*15,'Printing using String Variables',"="*15)
+message = "Hello World"
+print (message)
+print (message, message, " this print statement is print 3 things\n")
 
 
 print("="*15,'Individual Character printing','='*15)
+print (message[0])
+print (message[0:7])
+print (message[-1])
+print (len(message))
+print (len("message"))
+print (message.count("World"))
 
 
 print("="*15,'Replacing Characters or Words within an existing String',"="*15)
+new_message = message.replace("Hello", 'Goodbye')
+print (new_message)
+new_message = message.replace("Hello", 'Goodbye')
+print (new_message)
+
 
 
 print("="*15,'Manipulating String data Cont. (adding strings together)',"="*15)
