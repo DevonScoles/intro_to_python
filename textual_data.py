02b0f0718adc2a56787f6c122fb6881596f7c922
descoles <descoles@iu.edu>
2022-03-29 16:15:33 -0400

    final premeeting commit

---
+++ textual_data.py
@@ -0,0 +1,19 @@
+print('<'*25,"Begining",'>'*25) # use '\n' to create a new line inside of a string
+
+
+print("="*15,'Printing using String Variables',"="*15)
+
+
+print("="*15,'Individual Character printing','='*15)
+
+
+print("="*15,'Replacing Characters or Words within an existing String',"="*15)
+
+
+print("="*15,'Manipulating String data Cont. (adding strings together)',"="*15)
+
+
+
+
+
+print("="*15,'How is this important/useful???','='*15)
