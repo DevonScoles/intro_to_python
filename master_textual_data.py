1857dd043c0870a2cc8b6ab623d89d5d7acc2582
Devon Scoles <Scolesdevon@gmail.com>
2022-04-12 14:20:05 -0400

    complete file from 4/5 meeting

--- master_textual_data.py
+++ master_textual_data.py
@@ -33,3 +33,51 @@ new_message = message.replace('Hello', 'Goodbye')
 message = message.replace('Hello', 'Good Evening')
 print('This is our new message:', new_message)
 print('This is our redefined message variable:',message,'\n')
+
+print("="*15,'Manipulating String data Cont. (adding strings together)',"="*15)
+combined_message = message + ', ' + new_message
+print(combined_message)
+print(f'{message}, {new_message} and goodnight!')
+print('{}, {} And Goodnight!'.format(message, new_message),'\n')
+
+
+print("="*15,'Taking user input',"="*15)
+user_name = input('What is your name? ') 
+#the above line will prompt the user asking their name and assign whatever they type after the prompt to the variable user_name
+user_sirname = input()
+#above line will take user input without displaying any prompt
+print(user_name, user_sirname)
+
+print("="*15,'How is this important/useful???','='*15)
+# Below is a function I prepared for a tech interview for a company I'll be working for this summer
+# in New York City and New Haven Connecticut.
+
+def madLib(): #this is a function called MadLib that takes no parameters inside the parantheses
+
+    #print statement to say Madlib time!!! and then go down two lines
+    print("MadLib time!!!\n")
+
+    #these are variables that take ask you for words
+    pluralNoun = input("Give me a plural noun: ")
+    adjective = input("Now an adjective: ")
+    pluralAnimal = input("How about an animal: ")
+    adjAnimal = input("Now I need an adjective and animal: ")
+    animal2 = input("Another animal: ")
+    adjective2 = input("Okay now I need another adjective: ")
+    pluralAnimal = input("Now I need a plural animal: ")
+    adjective3 = input("Give me another adjective: ")
+    food = input('Whats a funky food?: ')
+    drink = input('Apple juice or gatorade?: ')
+    adjective4 = input("Give me another adjective: ")
+    pluralAnimal2 = input("Plural animal: ")
+    housing = input("A fun palce to live: ")
+    pluralAnimal3 = input("last but not least another plural animal: ")
+
+
+    #this is the print statement inside of the function you can tell because it looks indented
+    print(f'\nSo many {pluralNoun} make {adjective} pets. \nMany people like furry {pluralAnimal} or a {adjAnimal} to cuddle with. \
+    \nSome people like having a {animal2} for a pet, and others prefer {adjective2} {pluralAnimal}. \nMost pets need things like {adjective3} \
+    care, {food}, {drink}, exercise, and a {adjective4} place to sleep. \nSome pets like {pluralAnimal2} live in {housing},\
+    but everyone can agree that {pluralAnimal3} are cute and crazy') 
+
+madLib()
